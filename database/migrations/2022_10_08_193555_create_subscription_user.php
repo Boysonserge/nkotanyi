53d4bcb40277eaa6ecd2723c32533f3d017eec1d
Boysonserge <tuyishimeserge@gmail.com>
2022-10-08 22:17:36 +0200

    some new changes

---
+++ database/migrations/2022_10_08_193555_create_subscription_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id')->constrained()
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('user_id')->constrained()
+                ->cascadeOnDelete()->cascadeOnUpdate();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_user');
+    }
+};
